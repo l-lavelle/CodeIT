1abe878532ad059efd1fee23bfa755e2c6f87770
Lauren <lauren.m.lavelle@wmich.edu>
2024-04-30 21:25:03 -0400

    emailJS working for contact page

--- src/main/frontend/src/components/Contact/Contact.jsx
+++ src/main/frontend/src/components/Contact/Contact.jsx
@@ -1,40 +1,67 @@
-import React from "react";
 import '../../Variables.css'
 import './Contact.css'
 import circuitBoard from '../../assets/bg.jpg';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import  { useState } from 'react';
 
 const Contact = () => {
+    const [message, setMessage]=useState({message:'', status:''});
+    const [emailData, setEmailData] = useState({ message: '', from_name: '', from_email:''});
+
+    const updateData= async (event)=>{
+        const { name, value } = event.target;
+        setEmailData({ ...emailData, [name]: value });
+      };
+
+    const form = useRef();
+
+     const sendEmail = (e) => {
+       e.preventDefault();
+
+       emailjs.sendForm("service_il6b8zg", 'template_pb001mh', form.current, 'zhCenTCZqdBMtVbwy')
+         .then((result) => {
+            setMessage({message:'Email was sent', status:'success'})
+            setEmailData({
+                message: '',
+                from_name: '',
+                from_email:''
+              })
+         }, (error) => {
+            setMessage({message:'Unable to send email', status:'error'})
+         });
+     };
+
   return (
     <div className="contact-container-wrapper">
     <div className="contact-top-container"></div>
     <div className="contact-container p-4">
         <h2>Contact Us</h2>
-        <Form>
+        <Form ref={form} onSubmit={sendEmail}>
               <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text"  onChange={updateData} value={emailData.from_name} name="from_name"/>
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control type="email"  onChange={updateData} value={emailData.from_email} name="from_email"/>
               </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-              </Form.Group>
+
+               <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                  <Form.Label>Message</Form.Label>
+                  <Form.Control as="textarea" rows={8}  onChange={updateData} value={emailData.message} name="message"/>
+                </Form.Group>
+
               <Button variant="primary" type="submit">
                 Submit
               </Button>
             </Form>
+
+            {message.status==='error'?<p className='text-center mt-3' style={{color:"red"}}>{message.message}</p>:null}
+            {message.status==='success'?<p className='text-center mt-3' style={{color:"green"}}>{message.message}</p>:null}
     </div>
     <div className="contact-bottom-container"></div>
     </div>
