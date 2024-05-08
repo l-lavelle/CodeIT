a2b2b7d3691cf1d0b93b5a3924b14cf7480817a5
Lauren <lauren.m.lavelle@wmich.edu>
2024-05-07 20:19:09 -0400

    login and signup pages created

--- src/main/frontend/src/components/Login/Login.jsx
+++ src/main/frontend/src/components/Login/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import '../../Variables.css'
 import './Login.css'
 import Form from 'react-bootstrap/Form';
+import CodeCat from '../CodeCat/CodeCat';
 
 const Login = () => {
   const [switched, setSwitched] = useState(false);
@@ -11,13 +12,13 @@ const Login = () => {
   }
 
   return (
-<main>
+<main className="SL-wrapper">
+{ switched===true? <h2 className="pt-5">SignUp</h2>:<h2 className="pt-5">Login</h2> }
   <div className="p-3 signup-login-container">
     <div className={switched===true? "hidden ":"visible login-container"}>
-    <h2>Login</h2>
      <Form>
       <Form.Group className="mb-3 text-left signup-login-text" controlId="formBasic">
-        <Form.Label>Email address</Form.Label>
+        <Form.Label>Email Address</Form.Label>
         <Form.Control type="text" />
       </Form.Group>
 
@@ -25,20 +26,24 @@ const Login = () => {
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" />
       </Form.Group>
+     <button className="button-ct" variant="primary" type="submit">
+          Submit
+     </button>
     </Form>
     </div>
-    <div className={switched===true? "hidden":"visible login-switch-container"}>
-     <button variant="primary" type="button" onClick={fadeOut}>
-            Sign Up switch
-     </button>
+    <div className={switched===true? "hidden":"visible signup-switch-container"}>
+     <p>
+            Don't have an account yet? <span className="SL-switch-text" onClick={()=>setSwitched(true)}>Signup</span>
+     </p>
+    <CodeCat />
     </div>
 
 
-  <div className={switched===true? "visible":"hidden"}>
-    <h2>Sign Up</h2>
+  <div className={switched===true? "visible signup-container":"hidden"}>
+
          <Form>
           <Form.Group className="mb-3 text-left signup-login-text" controlId="formBasic">
-            <Form.Label>Email address</Form.Label>
+            <Form.Label>Email Address</Form.Label>
             <Form.Control type="text" />
           </Form.Group>
 
@@ -51,12 +56,16 @@ const Login = () => {
               <Form.Label>Confirm Password</Form.Label>
               <Form.Control type="password" />
           </Form.Group>
-
-          <button variant="primary" type="button" onClick={()=>setSwitched(false)}>
-            Sign Up
-          </button>
         </Form>
-    </div>
+        <button className="button-ct" variant="primary" type="submit">
+             Submit
+        </button>
+        </div>
+        <div className={switched===true? "visible login-switch-container":"hidden"}>
+        <p> Already have an account? <span className="SL-switch-text" onClick={()=>setSwitched(false)}>Login</span>
+         </p>
+        <CodeCat />
+      </div>
    </div>
 </main>
   );
